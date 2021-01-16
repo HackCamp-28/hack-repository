b9c99d675986df7a0075a3de084456c43147f14a
silverbugs <77442276+silverbugs@users.noreply.github.com>
2021-01-16 12:08:40 +0000

    Add files via upload

---
+++ DataSet.php
@@ -0,0 +1,36 @@
+<?php
+//class for handling operations on all 3 databases
+require_once ('Database.php');
+
+class DataSet{
+    protected $_dbHandle, $_dbInstance;
+        
+    public function __construct() { //constructor
+        $this->_dbInstance = Database::getInstance();
+        $this->_dbHandle = $this->_dbInstance->getdbConnection();
+    }
+    
+
+    public function verifyUserCredentials($username, $password){ //checks the database for an account with the matching username and password given
+        $sqlQuery = "SELECT * FROM Users WHERE username = ? AND password = ?;";
+
+        $statement = $this->_dbHandle->prepare($sqlQuery);
+        $statement->execute([$username,$password]);
+
+        $dataSet = Array();
+        while ($row = $statement->fetch()) {
+            $dataSet[] = $row;
+        }
+
+        //returns true or false depending on whether the account was found or not.
+        if(empty($dataSet)){
+            return false;
+
+        }
+        else{
+            $_SESSION["userDetails"] = $dataSet;
+            return true;
+
+        }
+
+}}
